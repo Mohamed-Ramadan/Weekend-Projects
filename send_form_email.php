8d79e8116283f8796f8da0c95562eb6efa9fc308
Mohamed-Ramadan <mohamed_r_2015@yahoo.com>
2015-08-16 23:01:49 +0300

    contact us form

---
+++ send_form_email.php
@@ -0,0 +1,155 @@
+﻿<?php
+ 
+if(isset($_POST['email'])) {
+ 
+     
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+ 
+    $email_to = "hdpt@hvac-iec.com";
+ 
+    $email_subject = "Contact US Email";
+ 
+     
+	function phpAlert($msg) {
+		echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+		exit;
+	}
+     
+ 
+    function died($error) {
+ 
+        // your error code can go here
+ 
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+ 
+        echo "These errors appear below.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "يمكنك الرجوع لتصحيح الخطأ<br /><br />";
+        
+        die();
+ 
+    }
+ 
+	
+    // validation expected data exists
+	
+    if(!isset($_POST['first_name']) ||
+ 
+        !isset($_POST['last_name']) ||
+ 
+        !isset($_POST['nID']) ||
+ 
+		!isset($_POST['job']) ||
+		
+		!isset($_POST['email']) ||
+        
+		!isset($_POST['telephone']) ||
+		
+		!isset($_POST['department']) ||
+		
+        !isset($_POST['comments'])) {
+ 
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+ 
+    }
+ 
+     
+ 
+    $first_name = $_POST['first_name']; // required
+ 
+    $last_name = $_POST['last_name']; // required
+ 
+	$nID = $_POST['nID']; // required
+	
+	$job = $_POST['job']; // required
+	
+	$department = $_POST['department']; // required
+	
+    $email_from = $_POST['email']; // required
+ 
+    $telephone = $_POST['telephone']; // not required
+ 
+    $comments = $_POST['comments']; // required
+	
+	
+    $error_message = "";
+ 
+    //$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+    //$string_exp = "/^[A-Za-z .'-]+$/";
+	//$number_exp = "/[0-9]+$/";
+	
+ 
+  if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+     
+ 
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+ 
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+ 
+	$email_message .= "nID: ".clean_string($nID)."\n";
+	
+	$email_message .= "Job: ".clean_string($job)."\n";
+	
+	$email_message .= "department: ".clean_string($department)."\n";
+	
+	$email_message .= "Email: ".clean_string($email_from)."\n";
+    
+	$email_message .= "Telephone: ".clean_string($telephone)."\n";
+ 
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+ 
+     
+ 
+     
+ 
+// create email headers
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+  mail($email_to, $email_subject, $email_message, $headers);  
+
+	echo "<script> 
+		alert('تم إراسال الرسالة بنجاح , شكرا لتواصلك معنا , سوف نتواصل معك قريبا.');
+		window.location.href='contactform.html';
+		</script>";
+	
+?>
+ 
+ 
+ 
+<!-- include your own success html here -->
+ 
+
+ 
+ 
+
+ 
+ 
+ 
+<?php
+ 
+}
+ 
+?>
---
+++ validateForm.js
@@ -0,0 +1,168 @@
+﻿
+function validateForm()
+{
+	var goodColor = "#66cc66";
+	var badColor = "#800000";
+	whiteColor = "#ffffff";
+
+  	var anything = document.forms["contactform"]["first_name"].value;
+
+	if(anything == null || anything == "")
+	{
+		document.forms["contactform"]["first_name"].style.background = badColor;
+		//document.getElementById("error!").innerHTML ="*Please inter a First Name*";
+		alert("الرجاء إدخال الاسم الاول");
+		return false;
+	}		
+	else{
+		document.forms["contactform"]["first_name"].style.background = whiteColor;		
+	}
+
+//---------------------------------------------------------------
+
+  	var anything = document.forms["contactform"]["last_name"].value;
+
+	if(anything == null || anything == "")
+	{
+		document.forms["contactform"]["last_name"].style.background = badColor;
+		alert("الرجاء إدخال الاسم الاخير");
+		return false;
+	}		
+	else{
+		document.forms["contactform"]["last_name"].style.background = whiteColor;		
+	}
+
+//----------------------------------------------------------------
+
+  	var anything = document.forms["contactform"]["nID"].value;
+
+	if(anything == null || anything == "")
+	{
+		document.forms["contactform"]["nID"].style.background = badColor;
+		alert("الرجاء إدخال الرقم القومى");
+		return false;
+	}		
+	else{
+		document.forms["contactform"]["nID"].style.background = whiteColor;		
+	}
+	
+	var test = anything;
+	for(var i = 0; i < test.length; i++)
+	{
+		var c = test.substring(i, i+1);
+		if(c != "0" && c != "1" &&  c != "2" &&  c != "3" &&  c != "4"
+		    &&  c != "5" &&  c != "6" &&  c != "7" &&  c != "8" &&  c != "9")
+		{
+		document.forms["contactform"]["nID"].style.background = badColor;
+		alert("الرجاء إدخال الرقم القومى أرقام فقط");
+		return false;
+		}
+	}
+
+//-----------------------------------------------------------------------------
+	var anything = document.forms["contactform"]["telephone"].value;
+	
+	if(anything == null || anything == "")
+	{
+		document.forms["contactform"]["telephone"].style.background = badColor;
+		alert("الرجاء إدخال رقم التليفون");
+		return false;
+	}		
+	else{
+		document.forms["contactform"]["telephone"].style.background = whiteColor;		
+	}
+	
+	var test = anything
+	
+	for(var i = 0; i < test.length; i++)
+	{
+		var c = test.substring(i, i+1);
+		if(c != "0" && c != "1" &&  c != "2" &&  c != "3" &&  c != "4"
+		    &&  c != "5" &&  c != "6" &&  c != "7" &&  c != "8" &&  c != "9")
+		{
+			document.forms["contactform"]["telephone"].style.background = badColor;
+			alert("الرجاء إدخال رقم التليفون أرقام فقط");
+			return false;
+		}
+	}
+	
+	
+
+//---------------------------------------------------------------
+
+  	var anything = document.forms["contactform"]["job"].value;
+
+	if(anything == null || anything == "")
+	{
+		document.forms["contactform"]["job"].style.background = badColor;
+		alert("الرجاء إدخال الوظيفة");
+		return false;
+	}
+	else{
+		document.forms["contactform"]["job"].style.background = whiteColor;		
+	}
+
+//----------------------------------------------------------
+
+  	var anything = document.forms["contactform"]["email"].value;
+	var atpos = anything.indexOf("@");
+	var dotpos = anything.lastIndexOf(".");
+
+	if(anything == null || anything == "")
+	{
+		document.forms["contactform"]["email"].style.background = badColor;
+		alert("الرجاء إدخال البريد الالكترونى");
+		return false;
+	}
+	else{
+		document.forms["contactform"]["email"].style.background = whiteColor;		
+	}
+
+	if(atpos < 1 || dotpos < atpos+2 || dotpos+2 >= anything.length)		
+	{
+		document.forms["contactform"]["email"].style.background = badColor;
+		alert("هذا البريد الالكترونى غير صحيح , من فضلك ادخل بريد الكترونى صحيح");
+		return false;
+	}
+	else{
+		document.forms["contactform"]["email"].style.background = whiteColor;		
+	}
+//----------------------------------------------------------------
+
+
+  	var anything = document.forms["contactform"]["department"].value;
+
+	if(anything == null || anything == "")
+	{
+		document.forms["contactform"]["department"].style.background = badColor;
+		alert("الرجاء إدخال القسم");
+	return false;
+	}
+	else{
+		document.forms["contactform"]["department"].style.background = whiteColor;		
+	}
+	
+//----------------------------------------------------------------
+
+
+  	var anything = document.forms["contactform"]["comments"].value;
+
+	if(anything == null || anything == "")
+	{
+		//document.forms["contactform"]["comments"].style.background = badColor;
+		alert("الرجاء ادخال الموضوع");
+		return false;
+	}
+	else{
+		document.forms["contactform"]["comments"].style.background = whiteColor;		
+	}
+	
+	if(anything.length < 2){
+		document.forms["contactform"]["comments"].style.background = badColor;
+		alert("من فضلك ادخل عدد حروف لا يقل عن 2 حرف");
+		return false;
+	}
+
+//--------------------------------------------------------------------	
+
+}
